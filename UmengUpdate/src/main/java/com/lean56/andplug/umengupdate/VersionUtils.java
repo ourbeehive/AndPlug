89a08931331640cc3f9e1c8654720c6f5fbce9af
Charles <zhangchaoxu@gmail.com>
2015-06-25 09:49:15 +0800

    #3 add umeng update jar and so

---
+++ UmengUpdate/src/main/java/com/lean56/andplug/umengupdate/VersionUtils.java
@@ -0,0 +1,42 @@
+package com.lean56.andplug.umengupdate;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Version Utils
+ *
+ * @author Charles
+ */
+public class VersionUtils {
+
+    /**
+     * get the current version
+     */
+    public static String getCurrentVersion(Context context) {
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info.versionName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * get the current versionCode
+     */
+    public static int getCurrentVersionCode(Context context) {
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info.versionCode;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+}
