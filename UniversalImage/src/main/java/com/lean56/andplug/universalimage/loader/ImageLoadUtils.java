a9a0f7bf1483c0dd526f775e30d9f0258843f529
Charles <zhangchaoxu@gmail.com>
2015-06-25 10:13:01 +0800

    edit universal image class

---
+++ UniversalImage/src/main/java/com/lean56/andplug/universalimage/loader/ImageLoadUtils.java
@@ -0,0 +1,28 @@
+package com.lean56.andplug.universalimage.loader;
+
+import android.widget.ImageView;
+import com.lean56.andplug.universalimage.R;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+/**
+ * Image Load Utils
+ *
+ * @author Charles(zhangchaoxu@gmail.com)
+ */
+public class ImageLoadUtils {
+    public ImageLoader imageLoader = ImageLoader.getInstance();
+
+    public static DisplayImageOptions options = new DisplayImageOptions.Builder()
+            .showImageOnLoading(R.drawable.ic_default_user)
+            .showImageForEmptyUri(R.drawable.ic_default_user)
+            .showImageOnFail(R.drawable.ic_default_user)
+            .cacheInMemory(true)
+            .cacheOnDisk(true)
+            .considerExifParams(true)
+            .build();
+
+    public void loadImage(ImageView imageView, String url) {
+        imageLoader.displayImage(url, imageView, options);
+    }
+}
