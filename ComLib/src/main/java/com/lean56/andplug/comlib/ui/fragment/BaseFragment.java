ed62983abe84d608b3929bc1663a4799dbcec7c5
Charles <zhangchaoxu@gmail.com>
2015-06-30 23:18:36 +0800

    add basefragment

---
+++ ComLib/src/main/java/com/lean56/andplug/comlib/ui/fragment/BaseFragment.java
@@ -0,0 +1,21 @@
+package com.lean56.andplug.comlib.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Base Fragment
+ *
+ * @author Charles
+ */
+public class BaseFragment extends Fragment {
+
+    /**
+     * Is this fragment usable from the UI-thread
+     *
+     * @return true if usable, false otherwise
+     */
+    protected boolean isUsable() {
+        return getActivity() != null;
+    }
+
+}
