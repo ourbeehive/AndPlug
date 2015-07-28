9a15a14e2c4700f2c04f36966423ff2f36f40b17
Charles <zhangchaoxu@gmail.com>
2015-07-28 09:34:51 +0800

    add login activity to App

---
+++ App/src/main/java/com/lean56/andplug/app/AppContext.java
@@ -0,0 +1,11 @@
+package com.lean56.andplug.app;
+
+import com.lean56.andplug.comlib.BaseApplication;
+
+/**
+ * App Application
+ *
+ * @author Charles
+ */
+public class AppContext extends BaseApplication {
+}
