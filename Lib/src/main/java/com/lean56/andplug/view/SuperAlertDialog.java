79b36e4fb46da4380a27df6ab300548b6b867ca9
Charles <zhangchaoxu@gmail.com>
2015-11-22 21:10:51 +0800

    wrapper dialog

---
+++ Lib/src/main/java/com/lean56/andplug/view/SuperAlertDialog.java
@@ -0,0 +1,12 @@
+package com.lean56.andplug.view;
+
+import com.afollestad.materialdialogs.AlertDialogWrapper;
+
+/**
+ * Migration from AlertDialogs
+ * support by https://github.com/afollestad/material-dialogs
+ *
+ * @author Charles
+ */
+public class SuperAlertDialog extends AlertDialogWrapper {
+}
