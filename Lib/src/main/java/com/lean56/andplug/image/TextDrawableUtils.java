7a7667ced4af41387caa36c8729387613494ced7
Charles <zhangchaoxu@gmail.com>
2015-12-15 16:08:21 +0800

    change layout of rteplan view

--- Lib/src/main/java/com/lean56/andplug/image/TextDrawableUtils.java
+++ Lib/src/main/java/com/lean56/andplug/image/TextDrawableUtils.java
@@ -11,16 +11,32 @@ import com.lean56.andplug.image.view.TextDrawable;
  */
 public class TextDrawableUtils {
 
-    public static TextDrawable genFirstCharTextDrawable(String str) {
-        return genFirstCharTextDrawable(str, Color.LTGRAY);
+    public enum SHAPE {
+        RECT, ROUND, ROUND_RECT
     }
 
-    public static TextDrawable genFirstCharTextDrawable(String str, int color) {
-        String text = "";
-        if(!TextUtils.isEmpty(str)) {
-            text = str.substring(0, 1);
+    private static int DEFAULT_BG_COLOR = Color.LTGRAY;
+
+    public static TextDrawable genTextDrawable(String str) {
+        return genTextDrawable(str, DEFAULT_BG_COLOR, SHAPE.RECT);
+    }
+
+    public static TextDrawable genTextDrawable(String str, SHAPE shape) {
+        return genTextDrawable(str, DEFAULT_BG_COLOR, shape);
+    }
+
+    public static TextDrawable genTextDrawable(String str, int color) {
+        return genTextDrawable(str, color, SHAPE.RECT);
+    }
+
+    public static TextDrawable genTextDrawable(String str, int color, SHAPE shape) {
+        String text = TextUtils.isEmpty(str) ? "" : str.substring(0, 1);
+        if (shape == SHAPE.ROUND) {
+            return TextDrawable.builder().buildRound(text, color);
+        } else if (shape == SHAPE.ROUND_RECT){
+            return TextDrawable.builder().buildRoundRect(text, color, 8);
+        } else {
+            return TextDrawable.builder().buildRect(text, color);
         }
-        return TextDrawable.builder().buildRound(text, color);
     }
-
 }
